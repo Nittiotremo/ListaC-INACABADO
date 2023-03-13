b90b850eb4c791240dad19dec0e5e6a7a262b3da
Carlos <120525900+Nittiotremo@users.noreply.github.com>
2023-03-13 20:27:43 -0300

    Add files via upload

---
+++ ListaC-INACABADO-main/Ex_forms1/Ex_forms1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Ex_forms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.Text2 = new System.Windows.Forms.TextBox();
+            this.Somar = new System.Windows.Forms.Button();
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Forms1";
+            // 
+            // Text1
+            // 
+            this.Text1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Text1.Location = new System.Drawing.Point(57, 93);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(129, 20);
+            this.Text1.TabIndex = 1;
+            // 
+            // Text2
+            // 
+            this.Text2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Text2.Location = new System.Drawing.Point(57, 119);
+            this.Text2.Name = "Text2";
+            this.Text2.Size = new System.Drawing.Size(129, 20);
+            this.Text2.TabIndex = 2;
+            // 
+            // Somar
+            // 
+            this.Somar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Somar.Location = new System.Drawing.Point(57, 181);
+            this.Somar.Name = "Somar";
+            this.Somar.Size = new System.Drawing.Size(104, 23);
+            this.Somar.TabIndex = 3;
+            this.Somar.Text = "Somar";
+            this.Somar.UseVisualStyleBackColor = true;
+            this.Somar.Click += new System.EventHandler(this.Somar_Click);
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_resultado.Location = new System.Drawing.Point(57, 145);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.Size = new System.Drawing.Size(129, 20);
+            this.txt_resultado.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor 1";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Valor 2";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Subtrair";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Dividir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(167, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "multiplicar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // limpar
+            // 
+            this.limpar.Location = new System.Drawing.Point(57, 239);
+            this.limpar.Name = "limpar";
+            this.limpar.Size = new System.Drawing.Size(104, 23);
+            this.limpar.TabIndex = 11;
+            this.limpar.Text = "limpar";
+            this.limpar.UseVisualStyleBackColor = true;
+            this.limpar.Click += new System.EventHandler(this.limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 294);
+            this.Controls.Add(this.limpar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_resultado);
+            this.Controls.Add(this.Somar);
+            this.Controls.Add(this.Text2);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Text1;
+        private System.Windows.Forms.TextBox Text2;
+        private System.Windows.Forms.Button Somar;
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button limpar;
+    }
+}
+
---
+++ ListaC-INACABADO-main/Ex_forms1/Ex_forms1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex_forms1
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Somar_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text = (float.Parse(Text1.Text) + float.Parse(Text2.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text = (float.Parse(Text1.Text) / float.Parse(Text2.Text)).ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text = (float.Parse(Text1.Text) - float.Parse(Text2.Text)).ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Text = (float.Parse(Text1.Text) * float.Parse(Text2.Text)).ToString();
+
+        }
+
+        private void limpar_Click(object sender, EventArgs e)
+        {
+            Text1.Text = ("");
+            Text2.Text = ("");
+            txt_resultado.Text = ("");
+        }
+    }
+}
